06d04f3dcf77d426742743e9c32d89d7b30607fe
jodfedlet <prog33@ixcsoft.com.br>
2020-12-22 21:46:18 -0300

    Authentication using passport-local.

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import ScheduleController from '../controllers/scheduleController';
 import UserController from '../controllers/userController';
 import OrganizationController from '../controllers/organizationController';
+// @ts-ignore
+import passport from 'passport';
 
 const router = Router();
 
@@ -19,6 +21,11 @@ router.post('/organization/register', (req, res) => {
 
 router.get('/schedule/list-dates', (req, res) => {
     ScheduleController.listSchedule(req, res)
+});
+
+router.post('/user/login', (req, res) => {
+    passport.authenticate('local', { failureRedirect: '/login' }),
+    UserController.logon(req, res)
 });
 
 export default router;
